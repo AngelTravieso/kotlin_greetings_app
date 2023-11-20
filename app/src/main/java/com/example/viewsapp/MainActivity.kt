5756023486f9023e1e3b77f9b9ab4e84f1f2df5d
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-20 16:30:25 -0400

    Buttons

--- app/src/main/java/com/example/viewsapp/MainActivity.kt
+++ app/src/main/java/com/example/viewsapp/MainActivity.kt
@@ -2,20 +2,35 @@ package com.example.viewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // EditText
         val et: EditText = findViewById(R.id.editText1)
         // val ett = findViewById<EditText>(R.id.editText1)
 
-        // Getting the input from EditText
-        val editTextInput: String = et.text.toString()
+        // Button
+        val btn: Button = findViewById(R.id.button)
 
-        println(editTextInput)
+        // Handle the click event on the button
+        btn.setOnClickListener() {
+            // Execute this code when the button is clicked
+
+            // Getting the input from EditText
+            val editTextInput: String = et.text.toString()
+
+            Toast.makeText(
+                this@MainActivity,
+                "$editTextInput",
+                Toast.LENGTH_LONG)
+                .show()
+        }
 
     }
 }
